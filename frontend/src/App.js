a679bb4de44b092d012c597c9854bdaa7327dcb2
abdul-naveed-git <skabdulnaveed0675@gmail.com>
2025-09-07 16:40:04 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
-import "./style.css";
+import "./styles.css";
 
 const BB84Simulator = () => {
   const [n, setN] = useState(8);
